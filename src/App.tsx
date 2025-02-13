470623ff51c724fad8e27bee44cd45f86db11963
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-13 15:47:14 -0300

    new: responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+/**
+* Autor: José Segura
+* GitHub: https://github.com/GKsegura
+* Data: 2025
+*/
+
 import { Header } from './components/Header';
 import TaskList from './components/Task/TaskList';
 import './global.css';
